71e3ec535ca6c9acda7e45a722b375c2dbec8689
Khatijahwys <84234@siswa.unimas.my>
2025-04-28 18:23:45 +0800

    added the home page under the lib' file

--- lib/Screen/home_page.dart
+++ lib/Screen/home_page.dart
@@ -1,24 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Skeleton',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  const HomePage(),
-    );
-  }
-}
-
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
